853e448260f46c784ccae3fda6768f2cfe1f8c6f
Jordan Cooper <looneybrains@cooperplanet.com>
2012-10-07 17:12:48 -0600

    Added some abilities

--- MyGame/Ability.cs
+++ MyGame/Ability.cs
@@ -13,12 +13,18 @@
         public readonly bool CausesTurnLoss;
         public readonly bool DestroyAllItems;
         public readonly bool MoveTarget;
+        public readonly bool AffectsAdjacentUnits;
         public readonly short ChanceToCauseDamage;
         public readonly short ChanceToFail;
         public readonly short ChanceToInfatuate;
         public readonly short ChanceToMiss;
         public readonly short ChanceToSnapOut;
         public readonly short ChanceToThaw;
+        public readonly short ChanceToRepeat;
+        public readonly short ChanceToAttackAgain;
+        public readonly short ChanceToReflectAttacks;
+        public readonly short ChanceToReflectSpells;
+        public readonly short ChanceToDelayAttack;
 
         public Ability(
             string name,
@@ -32,32 +38,46 @@
             bool causesTurnLoss = false,
             bool destroyAllItems = false,
             bool moveTarget = false,
+            bool affectsAdjacentUnits = false,
             short chanceToCauseDamage = 0,
             short chanceToFail = 0,
             short chanceToInfatuate = 0 ,
             short chanceToMiss = 0,
             short chanceToSnapOut = 0,
-            short chanceToThaw = 0
+            short chanceToThaw = 0,
+            short chanceToRepeat = 0,
+            short chanceToAttackAgain = 0,
+            short chanceToReflectAttacks = 0,
+            short chanceToReflectSpells = 0,
+            short chanceToDelayAttack = 0
+            
             )
-        {
-            Name = name;
-            Description = description;
-            Damage = damage;
-            Heal = heal;
-            CanTargetAlly = canTargetAlly;
-            CanTargetEnemy = canTargetEnemy;
-            Invulnerability = invulnerability;
-            AppliedOverTurns = appliedOverTurns;
-            CausesTurnLoss = causesTurnLoss;
-            CausesTurnLoss = causesTurnLoss;
-            DestroyAllItems = destroyAllItems;
-            MoveTarget = moveTarget;
-            ChanceToFail = chanceToFail;
-            ChanceToCauseDamage = chanceToCauseDamage;
-            ChanceToMiss = chanceToMiss;
-            ChanceToSnapOut = chanceToSnapOut;
-            ChanceToThaw = chanceToThaw;
-            ChanceToInfatuate = chanceToInfatuate;
-        }
+
+    {
+        Name = name;
+        Description = description;
+        Damage = damage;
+        Heal = heal;
+        CanTargetAlly = canTargetAlly;
+        CanTargetEnemy = canTargetEnemy;
+        Invulnerability = invulnerability;
+        AppliedOverTurns = appliedOverTurns;
+        CausesTurnLoss = causesTurnLoss;
+        CausesTurnLoss = causesTurnLoss;
+        DestroyAllItems = destroyAllItems;
+        AffectsAdjacentUnits = affectsAdjacentUnits;
+        MoveTarget = moveTarget;
+        ChanceToFail = chanceToFail;
+        ChanceToCauseDamage = chanceToCauseDamage;
+        ChanceToMiss = chanceToMiss;
+        ChanceToSnapOut = chanceToSnapOut;
+        ChanceToThaw = chanceToThaw;
+        ChanceToRepeat = chanceToRepeat;
+        ChanceToAttackAgain = chanceToAttackAgain;
+        ChanceToInfatuate = chanceToInfatuate;
+        ChanceToReflectAttacks = chanceToReflectAttacks;
+        ChanceToReflectSpells = chanceToReflectSpells;
+        ChanceToDelayAttack = chanceToDelayAttack;
+    }
     }
 }
--- MyGame/SpecialCondition.cs
+++ MyGame/SpecialCondition.cs
@@ -6,7 +6,7 @@ namespace MyGame
         public string Description { get; set; }
     }
     //Stun, Burn, Poison, BadlyPoison, Shock, Disorient, Freeze, Sleep, Regen, Bleed, Charm, Blind, Banish, Invulnerable
-
+    //MarkOfTheAssassin
     public class Stun : SpecialCondition
     {
         
